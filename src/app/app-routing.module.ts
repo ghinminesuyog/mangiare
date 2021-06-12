fbd8fcdee842767786563ee8b9dd8cd81c0adff5
ghinminesuyog <suyogghinmine@gmail.com>
2021-06-12 12:36:04 +0530

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookComponent } from './pages/book/book.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { PlaceComponent } from './pages/place/place.component';
+import { TeamComponent } from './pages/team/team.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'place',
+    component: PlaceComponent
+  },
+  {
+    path: 'book',
+    component: BookComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { PlaceComponent } from './pages/place/place.component';
+import { TeamComponent } from './pages/team/team.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { AboutComponent } from './components/about/about.component';
+import { ConnectComponent } from './components/connect/connect.component';
+import { DiscoverComponent } from './components/discover/discover.component';
+import { BookComponent } from './pages/book/book.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    PlaceComponent,
+    TeamComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroComponent,
+    AboutComponent,
+    ConnectComponent,
+    DiscoverComponent,
+    BookComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
